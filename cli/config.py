a58109fb111a69613198a896523c7a4f2821bc69
JakeLehle <jakelehle@gmail.com>
2022-03-11 16:50:52 -0600

    Peakpuller v0.1.0 Click CLI Updated

--- cli/config.py
+++ cli/config.py
@@ -17,27 +17,19 @@ def get_default_optional_parameters():
 
         return default_optionals
 
-def get_default_config(experiment_title, experiment_description, transcription_factor_or_histone_chip, aligner, duplication_marker, genome_tsv, experimental_reads_are_paired_end, control_reads_are_paired_end, always_use_pooled_ctl, experimental_rep_R1, experimental_rep_R2, control_rep_R1, control_rep_R2):
+def get_default_config(cores_per_job, output_dir):
 
     mandatory_parameters = {
-        'rawdir': os.path.abspath(fastq_dir),
         'output_dir': os.path.abspath(output_dir),
-        'group1': group1,
-        'group2': group2,
-        'samples': group1 + group2,
-        'ref': os.path.abspath(fasta_ref),
         'computing_threads': cores_per_job,
         'io_threads': cores_per_job
     }
 
     optional_parameters = get_default_optional_parameters()
 
-    reference_annotation_files = get_reference_annotation_files(genome_build)
-
-    methylseekr_calibration_chr = get_methylseekr_calibration_chromosome(genome_build)
-
     return {
-        **mandatory_parameters
+        **mandatory_parameters,
+        **optional_parameters
     }
 
 
@@ -48,11 +40,8 @@ def dump_config(config_dict, target_file):
         yaml.dump(config_dict, f)
 
 
-def create_config(use_sample_files, genome_build, cores_per_job, fastq_dir, reference_fasta, group1, group2, output_dir, target_yaml):
+def create_config(cores_per_job, output_dir, target_yaml):
 
-    samples_in_group1 = group1.split(',')
-    samples_in_group2 = group2.split(',')
-
-    config_yaml = get_default_config(experiment_title, experiment_description, transcription_factor_or_histone_chip, aligner, duplication_marker, genome_tsv, experimental_reads_are_paired_end, control_reads_are_paired_end, always_use_pooled_ctl, experimental_rep_R1, experimental_rep_R2, control_rep_R1, control_rep_R2)
+    config_yaml = get_default_config(cores_per_job, output_dir)
 
     dump_config(config_yaml, target_yaml)
